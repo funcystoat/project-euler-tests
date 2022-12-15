162d0796fa42a6b100177de5183e411d8298d7a2
Alexander Eubanks <learningforfun@myyahoo.com>
2022-12-15 15:42:01 -0500

    Simplified Problem1's code

--- project.euler/src/main/java/problems/Problem1.java
+++ project.euler/src/main/java/problems/Problem1.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 public class Problem1 {
 	public static void main(String[] args) {
-		int upperBound = 1000;
+		int upperBound = 999;
 		
 		bruteForce(upperBound);
 		differentBruteForce(upperBound);
@@ -18,7 +18,7 @@ public class Problem1 {
 	public static void bruteForce(int upperBound) {
 		int sum = 0;
 
-		for (int i = 1; i < upperBound; i++) {
+		for (int i = 1; i <= upperBound; i++) {
 			if ((i % 3 == 0) || (i % 5 == 0))
 				sum += i;
 		}
@@ -42,8 +42,6 @@ public class Problem1 {
 			setOfMultiplesOf3or5.add(5 * i);
 		}
 
-		setOfMultiplesOf3or5.remove(upperBound);
-
 		Iterator<Integer> iter = setOfMultiplesOf3or5.iterator();
 
 		while (iter.hasNext())
@@ -55,9 +53,9 @@ public class Problem1 {
 	// Use the sum formula (sum i from 1 to N = N*(N+1)/2) to add up all
 	// multiples of 3, of 5, and then subtract the sum of all multiples of 15
 	public static void justUseMath(int upperBound) {
-		int N1 = (upperBound % 3 == 0) ? (upperBound / 3 - 1) : (upperBound / 3);
-		int N2 = (upperBound % 5 == 0) ? (upperBound / 5 - 1) : (upperBound / 5);
-		int N3 = (upperBound % 15 == 0) ? (upperBound / 15 - 1) : (upperBound / 15);
+		int N1 = upperBound / 3;
+		int N2 = upperBound / 5;
+		int N3 = upperBound / 15;
 
 		System.out.println(
 				3 * (N1 * (N1 + 1)) / 2 
